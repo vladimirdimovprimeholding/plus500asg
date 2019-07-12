7ab041f8c826014cea776f3d575795ac8570fdc8
Vladimir Dimov <vladimir.dimov@sbnd.net>
2019-07-12 08:37:18 +0300

    Added screens, some dimens and some styles

---
+++ app/src/main/java/com/vlado/plus500asg/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.vlado.plus500asg
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity;
+import android.view.View
+
+import kotlinx.android.synthetic.main.activity_main.*
+import android.support.v4.app.Fragment
+import com.vlado.plus500asg.buysell.BuySellFragment
+import com.vlado.plus500asg.list.ItemFragment
+import com.vlado.plus500asg.list.dummy.DummyContent
+
+
+class MainActivity : AppCompatActivity(), ItemFragment.OnListFragmentInteractionListener {
+
+
+    val fragmentBuySell: BuySellFragment = BuySellFragment()
+    val fragmentList: ItemFragment = ItemFragment()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onListFragmentInteraction(item: DummyContent.DummyItem?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    fun clickedBuySell(v: View){
+        removeAllFragments()
+        changeFragment(fragmentBuySell)
+    }
+
+    fun clickedList(v: View){
+        removeAllFragments()
+        changeFragment(fragmentList)
+    }
+
+    private fun changeFragment(fragment: Fragment){
+        val manager = supportFragmentManager
+        val transaction = manager.beginTransaction()
+        transaction.add(frame.id, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+    private fun removeAllFragments() {
+        val manager = supportFragmentManager
+        while (manager.getBackStackEntryCount() > 0) {
+            manager.popBackStackImmediate()
+        }
+    }
+}
